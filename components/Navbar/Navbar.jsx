6bc5c5ed2c3beb5b0a2c472c410382a5d82342fa
Bishesta Bohara <w10185873@usm.edu>
2025-04-04 18:42:09 -0500

    creating app

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+export default function Navbar(){
+    return (
+        <nav className="bg-pink-50 text-black p-4">
+           <ul className="flex justify-center space-x-50">
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+           </ul>
+        </nav>
+    );
+}
